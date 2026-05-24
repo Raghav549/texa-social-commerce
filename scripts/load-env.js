82199c5e383a4f3b9ca11865393778c9ee4e2522
Raghav kashyap <raghavkashyap2229@gmail.com>
2026-05-24 12:55:27 +0530

    Update load-env.js

--- scripts/load-env.js
+++ scripts/load-env.js
@@ -35,11 +35,9 @@ if (fs.existsSync(envPath)) {
 
 // Map system variables to Expo public variables
 const mappings = {
-  VITE_APP_ID: "EXPO_PUBLIC_APP_ID",
-  VITE_OAUTH_PORTAL_URL: "EXPO_PUBLIC_OAUTH_PORTAL_URL",
   OAUTH_SERVER_URL: "EXPO_PUBLIC_OAUTH_SERVER_URL",
-  OWNER_OPEN_ID: "EXPO_PUBLIC_OWNER_OPEN_ID",
-  OWNER_NAME: "EXPO_PUBLIC_OWNER_NAME",
+  EXPO_PUBLIC_API_URL: "EXPO_PUBLIC_API_URL",
+  EXPO_PUBLIC_SOCKET_URL: "EXPO_PUBLIC_SOCKET_URL",
 };
 
 for (const [systemVar, expoVar] of Object.entries(mappings)) {
